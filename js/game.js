3b8d17c5e6c2e133f6eb73578ab6cd6467d40f38
TomboFry <tombofry@hotmail.co.uk>
2017-03-25 19:14:11 +0000

    Add colour change and proper wall collisions
    Pigeon changes between blue, orange, and purple. As a result, the
    pigeon also gets blocked from going through walls that aren't its
    colour.
    
    Resized from 32px wide to 64px.
    
    Add .gitignore

--- js/game.js
+++ js/game.js
@@ -1,17 +1,24 @@
 var assetsObj = {
 	"sprites": {
-		// This spritesheet has 16 images, in a 2 by 8 grid
-		// The dimensions are 832x228
-		"images/spr_walk_strip11.png": {
-			// This is the width of each image in pixels
-			tile: 32,
-			// The height of each image
-			tileh: 34,
-			// We give names to three individual images
+		"images/spr_walk_orange.png": {
+			tile: 64,
+			tileh: 68,
 			map: {
-				walker_start: [0, 0],
-				walker_middle: [5, 0],
-				walker_end: [10, 0]
+				Orange: [0, 0],
+			}
+		},
+		"images/spr_walk_blue.png": {
+			tile: 64,
+			tileh: 68,
+			map: {
+				Blue: [0, 0],
+			}
+		},
+		"images/spr_walk_purple.png": {
+			tile: 64,
+			tileh: 68,
+			map: {
+				Purple: [0, 0],
 			}
 		}
 	}
@@ -23,13 +30,13 @@ window.onload = function() {
 };
 
 function game() {
-	var walker = Crafty.e('2D, Canvas, walker_start, SpriteAnimation, Collision, Gravity, Twoway')
+	var walker = Crafty.e('2D, Canvas, Purple, SpriteAnimation, Collision, Gravity, Twoway')
         .collision()
         .bind('Moved', function(e) {
             var hitDatas, hitData;
             if ((hitDatas = this.hit('Solid'))) {
                 hitData = hitDatas[0];
-                if (hitData.obj.has("Green") || hitData.obj.has("Floor")) {
+                if (!hitData.obj.has(walker.player_colour) || hitData.obj.has("Floor")) {
                     this._falling = false;
                     this.x -= hitData.overlap * hitData.normal.x;
                     this.y -= hitData.overlap * hitData.normal.y;
@@ -37,9 +44,41 @@ function game() {
             }
         })
         .twoway(200)
-        .gravity('Solid')
-	walker.reel("walking", 500, [
-		[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [8, 0], [10, 0]
-	]);
-	walker.animate("walking", -1);
+        .gravity('Floor')
+        .reel("walking", 500, [
+			[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0]
+		])
+		.animate("walking", -1)
+		.bind('KeyDown', function(e) {
+			if(e.key == Crafty.keys.Z) {
+				walker.removeComponent("Orange");
+				walker.removeComponent("Purple");
+				walker.addComponent("Blue");
+				walker.player_colour = "Blue";
+			} else if(e.key == Crafty.keys.X) {
+				walker.removeComponent("Purple");
+				walker.removeComponent("Blue");
+				walker.addComponent("Orange");
+				walker.player_colour = "Orange";
+			} else if(e.key == Crafty.keys.C) {
+				walker.removeComponent("Orange");
+				walker.removeComponent("Blue");
+				walker.addComponent("Purple");
+				walker.player_colour = "Purple";
+			}
+		});
+
+	walker.player_colour = "Purple";
+
+	console.log(walker);
+
+	Crafty.e("2D, Canvas, Solid, Floor, Collision, Color")
+		.attr({ x: 0, y: 350, w: 640, h: 10 })
+		.color('black')
+		.collision();
+
+	Crafty.e("2D, Canvas, Solid, Collision, Orange, Color")
+		.attr({ x: 480, y: 0, w: 20, h: 350 })
+		.color('orange')
+		.collision();
 }
