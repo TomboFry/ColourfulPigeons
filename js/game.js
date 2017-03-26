7abe61721d44b7d0ba1cacd8c4bc90ac0d696543
Jack Hair <jackhair@outlook.com>
2017-03-26 00:34:22 +0000

    Make pigeon moves on its own

--- js/game.js
+++ js/game.js
@@ -30,20 +30,23 @@ window.onload = function() {
 };
 
 function game() {
-	var walker = Crafty.e('2D, Canvas, Purple, SpriteAnimation, Collision, Gravity, Twoway')
+	var walker = Crafty.e('2D, Canvas, Purple, SpriteAnimation, Collision, Gravity, Jumper, Motion')
         .collision()
         .bind('Moved', function(e) {
             var hitDatas, hitData;
             if ((hitDatas = this.hit('Solid'))) {
                 hitData = hitDatas[0];
-                if (!hitData.obj.has(walker.player_colour) || hitData.obj.has("Floor")) {
-                    this._falling = false;
+                 if (hitData.obj.has("Floor")) {
+                    this.x -= hitData.overlap * hitData.normal.x;
+                    this.y -= hitData.overlap * hitData.normal.y;
+                } else if (!hitData.obj.has(walker.player_colour)) {
+                    this.velocity().x = 50;
                     this.x -= hitData.overlap * hitData.normal.x;
                     this.y -= hitData.overlap * hitData.normal.y;
                 }
             }
         })
-        .twoway(200)
+        .jumper(400, ['UP_ARROW'])
         .gravity('Floor')
         .reel("walking", 500, [
 			[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0]
@@ -69,6 +72,8 @@ function game() {
 		});
 
 	walker.player_colour = "Purple";
+    walker.velocity().x = 50;
+    walker.ax = 30;
 
 	Crafty.e("2D, Canvas, Solid, Floor, Collision, Color")
 		.attr({ x: 0, y: 350, w: 1280, h: 10 })
